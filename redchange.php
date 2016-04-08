2abb46a5dc34c4a2781c2992d4a901eb134383d6
Goryudyuma <umaretekyoumade@gmail.com>
2016-04-08 13:25:11 +0900

    画像の色を変更するツール

--- redchange.php
+++ redchange.php
@@ -1,10 +1,11 @@
 <?php
 
-$image = new Imagick('red.png');
+$image = new Imagick('triangle.png');
 //$clut = new Imagick();
 //$clut->newImage(1, 1, '#FF0000');
 //$image->clutImage($clut, imagick::CHANNEL_RED);
 //$image->paintOpaqueImage('red','green',1);
-$image->floodfillPaintImage('#0000FF', 1, $image->getImagePixelColor(1, 1), 1, 1, false);
+//$image->floodfillPaintImage('#0000FF', 1, $image->getImagePixelColor(1, 1), 1, 1, false);
+$image->paintOpaqueImage('#FFFFFF', '#00FFFF', 1);
 $image->writeImage('test_out.png');
 ?>
